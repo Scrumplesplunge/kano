53c435d225546d09b242bb747088931ad56ea026
Joe Fowler <scrumplesplunge@gmail.com>
2020-08-06 19:22:08 +0100

    Make the run script work with both bup and kano.

--- run.sh
+++ run.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-if [[ -z "$1" ]]; then
+filename="$1"
+
+if [[ -z "$filename" ]]; then
   >&2 echo 'Usage: run.sh <filename>'
   exit 1
 fi
 
-bin/debug/bup "$1" > /tmp/demo.s
+tool="${filename#*.}"
+
+"bin/debug/$tool" "$1" > /tmp/demo.s
 as --32 /tmp/demo.s -o /tmp/demo.o
 ld -m elf_i386 /tmp/demo.o -o /tmp/demo
 /tmp/demo
