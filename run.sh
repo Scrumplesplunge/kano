a19b0a7e23f81759acfee16031db54a1eee0a584
Joe Fowler <scrumplesplunge@gmail.com>
2020-08-06 19:47:43 +0100

    Make the run script bail out on errors.

--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@ fi
 
 tool="${filename#*.}"
 
-"bin/debug/$tool" "$1" > /tmp/demo.s
-as --32 /tmp/demo.s -o /tmp/demo.o
-ld -m elf_i386 /tmp/demo.o -o /tmp/demo
+"bin/debug/$tool" "$1" > /tmp/demo.s &&
+as --32 /tmp/demo.s -o /tmp/demo.o &&
+ld -m elf_i386 /tmp/demo.o -o /tmp/demo &&
 /tmp/demo
