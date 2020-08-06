cfda9df8ba8f5fa78e0f7fd8f4323138fb7c0a45
Joe Fowler <scrumplesplunge@gmail.com>
2020-08-06 19:19:09 +0100

    Split out kano and bup binaries.

---
+++ src/kano.cc
@@ -0,0 +1,17 @@
+#include <cassert>
+
+import <iostream>;
+import ir.parser;
+import ir.x86;
+import kano.ast;
+import kano.code;
+import kano.codegen;
+import kano.parser;
+
+int main(int argc, char* argv[]) {
+  if (argc != 2) {
+    std::cerr << "Usage: kano <filename>\n";
+    return 1;
+  }
+  ir::emit(std::cout, kano::compile(kano::parse(argv[1])));
+}
