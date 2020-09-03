f35244e94964b5941b0c71a473c4b356e99d9440
Joe Fowler <scrumplesplunge@gmail.com>
2020-09-03 21:23:17 +0100

    Make types comparable in the IR.

--- src/semantics/ir.cc
+++ src/semantics/ir.cc
@@ -33,16 +33,19 @@ struct array_type {
   std::uint64_t size;
   data_type element;
 };
+bool operator==(const array_type&, const array_type&);
 
 // Pointers to objects.
 struct pointer_type {
   data_type pointee;
 };
+bool operator==(const pointer_type&, const pointer_type&);
 
 // User-defined compound types.
 struct user_defined_type {
   symbol symbol;
 };
+bool operator==(user_defined_type, user_defined_type);
 
 // Function types. Note that function types are *not* data types! A function
 // cannot be copied, moved, stored in a class, etc. However, *pointers* to
@@ -51,12 +54,35 @@ struct function_type {
   data_type return_type;
   std::vector<data_type> parameters;
 };
+bool operator==(const function_type&, const function_type&);
 
 // Pointers to functions.
 struct function_pointer_type {
   function_type pointee;
 };
+bool operator==(const function_pointer_type&, const function_pointer_type&);
 
 using type = std::variant<data_type, function_type>;
+
+bool operator==(const array_type& l, const array_type& r) {
+  return l.size == r.size && l.element == r.element;
+}
+
+bool operator==(const pointer_type& l, const pointer_type& r) {
+  return l.pointee == r.pointee;
+}
+
+bool operator==(user_defined_type l, user_defined_type r) {
+  return l.symbol == r.symbol;
+}
+
+bool operator==(const function_type& l, const function_type& r) {
+  return l.return_type == r.return_type && l.parameters == r.parameters;
+}
+
+bool operator==(const function_pointer_type& l,
+                const function_pointer_type& r) {
+  return l.pointee == r.pointee;
+}
 
 }  // namespace semantics::ir
