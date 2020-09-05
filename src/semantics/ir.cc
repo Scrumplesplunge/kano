17fdbcc98b58befa4c56999c8827d1daf26d5164
Joe Fowler <scrumplesplunge@gmail.com>
2020-09-05 16:40:42 +0100

    More progress on checking.
    There's no specific milestone in this commit, but it compiles...

--- src/semantics/ir.cc
+++ src/semantics/ir.cc
@@ -15,7 +15,36 @@ export import <vector>;
 export namespace semantics::ir {
 
 // Symbols are unique identifiers for user-defined artefacts.
-enum symbol : int {};
+enum symbol : int {
+  none,
+  builtin_bool,
+  builtin_int32,
+  builtin_void,
+  builtin_void_load,
+  builtin_void_store,
+  builtin_bool_load,
+  builtin_bool_store,
+  builtin_bool_not,
+  builtin_bool_and,
+  builtin_bool_or,
+  builtin_bool_to_int32,
+  builtin_int32_load,
+  builtin_int32_store,
+  builtin_int32_to_bool,
+  builtin_int32_negate,
+  builtin_int32_add,
+  builtin_int32_subtract,
+  builtin_int32_multiply,
+  builtin_int32_divide,
+  builtin_int32_modulo,
+  builtin_int32_compare_eq,
+  builtin_int32_compare_ne,
+  builtin_int32_compare_lt,
+  builtin_int32_compare_le,
+  builtin_int32_compare_gt,
+  builtin_int32_compare_ge,
+  first_user_symbol,
+};
 
 enum builtin_type : int;
 struct array_type;
@@ -36,20 +65,23 @@ enum builtin_type : int {
 struct array_type {
   std::uint64_t size;
   data_type element;
+  bool operator==(const array_type&) const = default;
+  auto operator<=>(const array_type&) const = default;
 };
-bool operator==(const array_type&, const array_type&);
 
 // Pointers to objects.
 struct pointer_type {
   data_type pointee;
+  bool operator==(const pointer_type&) const = default;
+  auto operator<=>(const pointer_type&) const = default;
 };
-bool operator==(const pointer_type&, const pointer_type&);
 
 // User-defined compound types.
 struct user_defined_type {
   symbol symbol;
+  bool operator==(const user_defined_type&) const = default;
+  auto operator<=>(const user_defined_type&) const = default;
 };
-bool operator==(user_defined_type, user_defined_type);
 
 // Function types. Note that function types are *not* data types! A function
 // cannot be copied, moved, stored in a class, etc. However, *pointers* to
@@ -57,41 +89,41 @@ bool operator==(user_defined_type, user_defined_type);
 struct function_type {
   data_type return_type;
   std::vector<data_type> parameters;
+  bool operator==(const function_type&) const = default;
+  auto operator<=>(const function_type& o) const {
+    if (auto x = return_type <=> o.return_type; x != 0) return x;
+    return parameters == o.parameters
+               ? std::strong_ordering::equal
+               : parameters < o.parameters ? std::strong_ordering::less
+                                           : std::strong_ordering::greater;
+  }
 };
-bool operator==(const function_type&, const function_type&);
 
 // Pointers to functions.
 struct function_pointer_type {
   function_type pointee;
+  bool operator==(const function_pointer_type&) const = default;
+  auto operator<=>(const function_pointer_type&) const = default;
 };
-bool operator==(const function_pointer_type&, const function_pointer_type&);
 
 using type = std::variant<data_type, function_type>;
 
-bool operator==(const array_type& l, const array_type& r) {
-  return l.size == r.size && l.element == r.element;
-}
-
-bool operator==(const pointer_type& l, const pointer_type& r) {
-  return l.pointee == r.pointee;
-}
-
-bool operator==(user_defined_type l, user_defined_type r) {
-  return l.symbol == r.symbol;
-}
-
-bool operator==(const function_type& l, const function_type& r) {
-  return l.return_type == r.return_type && l.parameters == r.parameters;
-}
-
-bool operator==(const function_pointer_type& l,
-                const function_pointer_type& r) {
-  return l.pointee == r.pointee;
-}
-
 struct void_value {};
+struct pointer;
+struct function_pointer;
 struct array;
-using value = node<void_value, bool, std::int32_t, array>;
+using value =
+    node<void_value, bool, std::int32_t, pointer, function_pointer, array>;
+
+struct pointer {
+  symbol symbol;
+  data_type pointee_type;
+};
+
+struct function_pointer {
+  symbol symbol;
+  function_type pointee_type;
+};
 
 struct array {
   std::vector<value> contents;
@@ -102,6 +134,10 @@ data_type type_of(const value&);
 builtin_type type_of(const void_value&) { return builtin_type::void_type; }
 builtin_type type_of(bool) { return builtin_type::bool_type; }
 builtin_type type_of(std::int32_t) { return builtin_type::int32_type; }
+pointer_type type_of(const pointer& p) { return pointer_type{p.pointee_type}; }
+function_pointer_type type_of(const function_pointer& f) {
+  return function_pointer_type{f.pointee_type};
+}
 array_type type_of(const array& a) {
   DEBUG_ONLY {
     for (const auto& x : a.contents) {
@@ -116,5 +152,44 @@ data_type type_of(const value& v) {
     return {v.location(), type_of(x)};
   });
 }
+
+struct literal {
+  value value;
+};
+
+enum local : int {};
+
+struct constant {
+  value value;
+};
+
+struct stack_allocate {};
+
+struct call {
+  symbol op;
+  std::vector<local> arguments;
+};
+
+// Given a *T referring to an array of T, produces a *T referring to the nth
+// element of that array.
+struct index {
+  local address;
+  local offset;
+};
+
+using action = node<constant, stack_allocate, call, index>;
+
+struct step {
+  local destination;
+  action action;
+};
+
+struct expression {
+  // TODO: For supporting efficient move semantics, we need to track the value
+  // category for locals as well as their type. Value categories are not types:
+  // you can't have an lvalue array of prvalues or a prvalue array of lvalues.
+  std::map<local, data_type> locals;
+  std::vector<step> steps;
+};
 
 }  // namespace semantics::ir
--- src/syntax/check.cc
+++ src/syntax/check.cc
@@ -20,15 +20,22 @@ struct module_type {
   const environment* exports;
 };
 
+struct global {
+  semantics::ir::data_type type;
+};
+
+struct local {
+  semantics::ir::data_type type;
+};
+
 // Used for names which represent types instead of names which represent values
 // of a given type.
 struct type_type {
   semantics::ir::data_type type;
 };
 
-using name_type =
-    std::variant<semantics::ir::data_type, semantics::ir::function_type,
-                 module_type, type_type>;
+using name_type = std::variant<semantics::ir::function_type, module_type,
+                               global, local, type_type>;
 
 struct definition {
   // TODO: Add guts.
@@ -68,38 +75,67 @@ struct module_data {
   environment exports;
 };
 
+struct type_info {
+  // function load(address : *T) : T { ... }
+  semantics::ir::symbol load;
+  // function store(address : *T, value : T) { ... }
+  semantics::ir::symbol store;
+};
+
 struct checker {
-  semantics::ir::symbol next = {};
+  semantics::ir::symbol next_symbol = semantics::ir::symbol::first_user_symbol;
+  semantics::ir::local next_local = {};
   std::map<std::filesystem::path, module_data> modules;
+  // Map from structural type to its symbolic name. This is used for looking up
+  // operators for moving values of this type around.
+  std::map<semantics::ir::data_type, semantics::ir::symbol> type_by_structure;
+  // Map from symbolic type names to the table of operators for the type.
+  std::map<semantics::ir::symbol, type_info> types = {
+      {semantics::ir::symbol::builtin_void,
+       {.load = semantics::ir::symbol::builtin_void_load,
+        .store = semantics::ir::symbol::builtin_void_store}},
+      {semantics::ir::symbol::builtin_bool,
+       {.load = semantics::ir::symbol::builtin_bool_load,
+        .store = semantics::ir::symbol::builtin_bool_store}},
+      {semantics::ir::symbol::builtin_int32,
+       {.load = semantics::ir::symbol::builtin_int32_load,
+        .store = semantics::ir::symbol::builtin_int32_store}},
+  };
   environment builtins = {
-      .names = {
-          {"bool",
-           {"builtin",
-            {},
-            symbol(),
-            "bool",
-            type_type{{{}, semantics::ir::builtin_type::bool_type}}}},
-          {"int32",
-           {"builtin",
-            {},
-            symbol(),
-            "int32",
-            type_type{{{}, semantics::ir::builtin_type::int32_type}}}},
-          {"void",
-           {"builtin",
-            {},
-            symbol(),
-            "void",
-            type_type{{{}, semantics::ir::builtin_type::void_type}}}},
-      },
+      .names =
+          {
+              {"bool",
+               {"builtin",
+                {},
+                semantics::ir::symbol::builtin_bool,
+                "bool",
+                type_type{{{}, semantics::ir::builtin_type::bool_type}}}},
+              {"int32",
+               {"builtin",
+                {},
+                semantics::ir::symbol::builtin_int32,
+                "int32",
+                type_type{{{}, semantics::ir::builtin_type::int32_type}}}},
+              {"void",
+               {"builtin",
+                {},
+                semantics::ir::symbol::builtin_void,
+                "void",
+                type_type{{{}, semantics::ir::builtin_type::void_type}}}},
+          },
   };
 
   // Generate a new unique symbol for some exported artefact.
   semantics::ir::symbol symbol();
 
+  // Generate a new unique id for some local variable.
+  semantics::ir::local local();
+
   // Fetches data about a module. If the module is not yet processed, open the
   // file and process it. If it is already processed, return the existing data.
   const module_data& get_module(const std::filesystem::path& path);
+
+  const type_info& lookup_type(const semantics::ir::data_type&);
 };
 
 struct module_checker {
@@ -132,7 +168,63 @@ struct module_checker {
 
   semantics::ir::data_type check_type(const ast::expression&);
 
+  semantics::ir::expression check_expression(io::location,
+                                             const ast::identifier&);
+  semantics::ir::expression check_expression(const ast::expression&);
+
   const environment::name_info& resolve(const ast::expression&);
+};
+
+struct expression_checker {
+  module_checker& module;
+  semantics::ir::expression result = {};
+
+  using local_info =
+      std::pair<const semantics::ir::local, semantics::ir::data_type>;
+
+  struct info {
+    // Every value has a category which describes how that value can be used.
+    // This is distinct from the notion of a type, as the category is always
+    // implicit and never composes.
+    //
+    // An lvalue is a named quantity. The name can be read as `left value`, as
+    // if to say that an lvalue may appear on the left side of an assignment.
+    // However, this does not mean that all lvalues can be assigned to: an
+    // lvalue may still be of an unassignable type, such as a function.
+    //
+    // An rvalue is a pure value. For example, a literal integer is an rvalue.
+    enum { lvalue, rvalue } category;
+    const local_info* result;
+  };
+
+  const local_info& add(semantics::ir::data_type, semantics::ir::action);
+  const local_info& add(semantics::ir::value);
+  const local_info& alloc(semantics::ir::data_type);
+
+  info generate(io::location, const ast::identifier&);
+  info generate(io::location, const ast::literal_integer&);
+  info generate(io::location, const ast::literal_string&);
+  info generate(io::location, const ast::literal_aggregate&,
+                const semantics::ir::array_type&);
+  info generate(io::location, const ast::literal_aggregate&);
+  template <typename T>
+  info generate(io::location l, const T&) {
+    io::fatal_message{module.name(), l, io::message::error}
+        << "unimplemented expression type.";
+  }
+  info generate(const ast::expression&);
+
+  // Like generate, but instead of generating the value into a local, generate
+  // and store the value at the given address.
+  void generate_into(semantics::ir::local, io::location,
+                     const ast::literal_aggregate&,
+                     const semantics::ir::array_type&);
+  template <typename T>
+  void generate_into(semantics::ir::local, io::location l, const T&) {
+    io::fatal_message{module.name(), l, io::message::error}
+        << "unimplemented expression type.";
+  }
+  void generate_into(semantics::ir::local address, const ast::expression&);
 };
 
 const environment::name_info& environment::lookup(module_checker& module,
@@ -167,8 +259,14 @@ const environment::name_info& environment::define(
 }
 
 semantics::ir::symbol checker::symbol() {
-  const auto result = next;
-  next = semantics::ir::symbol((int)next + 1);
+  const auto result = next_symbol;
+  next_symbol = semantics::ir::symbol((int)next_symbol + 1);
+  return result;
+}
+
+semantics::ir::local checker::local() {
+  const auto result = next_local;
+  next_local = semantics::ir::local((int)next_local + 1);
   return result;
 }
 
@@ -179,6 +277,19 @@ const module_data& checker::get_module(const std::filesystem::path& path) {
   if (!is_new) return i->second;
   module_checker{*this, i->first, i->second}.check();
   return i->second;
+}
+
+const type_info& checker::lookup_type(const semantics::ir::data_type& d) {
+  const auto [i, is_new] =
+      type_by_structure.emplace(d, semantics::ir::symbol::none);
+  if (!is_new) return types.at(i->second);
+  i->second = symbol();
+  // TODO: Generate inline functions for each of these operations.
+  type_info info = {
+    .load = symbol(),
+    .store = symbol(),
+  };
+  return types.emplace(i->second, info).first->second;
 }
 
 std::string module_checker::name() const {
@@ -234,8 +345,14 @@ void module_checker::check(io::location l, const ast::import_statement& i) {
 
 const environment::name_info& module_checker::check(
     io::location l, const ast::variable_definition& v) {
-  // TODO: Check the initializer.
-  return environment.define(*this, l, v.id.value, check_type(v.type));
+  const auto& info =
+      environment.define(*this, l, v.id.value, global{check_type(v.type)});
+  if (v.initializer) {
+    expression_checker checker{*this};
+    checker.generate(*v.initializer);
+    // TODO: Consume the resulting expression.
+  }
+  return info;
 }
 
 const environment::name_info& module_checker::check(io::location l,
@@ -325,6 +442,22 @@ semantics::ir::data_type module_checker::check_type(
       << "unsupported type expression.";
 }
 
+semantics::ir::expression module_checker::check_expression(
+    io::location l, const ast::identifier& i) {
+  const auto& info = environment.lookup(*this, l, i.value);
+  if (const auto* type = std::get_if<global>(&info.type)) {
+
+  }
+  io::fatal_message{name(), l, io::message::error}
+      << "unimplemented.";
+}
+
+semantics::ir::expression module_checker::check_expression(
+    const ast::expression& e) {
+  io::fatal_message{name(), e.location(), io::message::error}
+      << "unimplemented.";
+}
+
 const environment::name_info& module_checker::resolve(
     const ast::expression& e) {
   if (const auto* i = e.get<ast::identifier>()) {
@@ -343,6 +476,160 @@ const environment::name_info& module_checker::resolve(
   // TODO: Implement support for nested types.
   io::fatal_message{name(), e.location(), io::message::error}
       << "unsupported scope expression.";
+}
+
+const expression_checker::local_info& expression_checker::add(
+    semantics::ir::data_type type, semantics::ir::action action) {
+  const auto id = module.program.local();
+  const auto [i, is_new] = result.locals.emplace(id, std::move(type));
+  result.steps.emplace_back(semantics::ir::step{id, std::move(action)});
+  return *i;
+}
+
+const expression_checker::local_info& expression_checker::add(
+    semantics::ir::value value) {
+  const auto location = value.location();
+  auto type = type_of(value);
+  return add(std::move(type),
+             {location, semantics::ir::constant{std::move(value)}});
+}
+
+const expression_checker::local_info& expression_checker::alloc(
+    semantics::ir::data_type type) {
+  return add(type, {type.location(), semantics::ir::stack_allocate{}});
+}
+
+expression_checker::info expression_checker::generate(
+    io::location location, const ast::identifier& i) {
+  // In the IR, we will represent variable references as pointers with an lvalue
+  // category.
+  const auto& info = module.environment.lookup(module, location, i.value);
+  if (const auto* f = std::get_if<semantics::ir::function_type>(&info.type)) {
+    const auto& result =
+        add({location, semantics::ir::function_pointer{info.symbol, *f}});
+    return {.category = info::lvalue, .result = &result};
+  }
+  if (const auto* g = std::get_if<global>(&info.type)) {
+    const auto& result =
+        add({location, semantics::ir::pointer{info.symbol, g->type}});
+    return {.category = info::lvalue, .result = &result};
+  }
+  if (const auto* l = std::get_if<local>(&info.type)) {
+    io::fatal_message{module.name(), location, io::message::error}
+        << "local variables are unimplemented.";
+  }
+  if (const auto* t = std::get_if<type_type>(&info.type)) {
+    io::message{module.name(), location, io::message::error}
+        << "a type may not appear in an expression.";
+    io::fatal_message{module.name(), location, io::message::note}
+        << "a value literal would have the syntax `type{...}`.";
+  }
+  io::fatal_message{module.name(), location, io::message::error}
+      << "unimplemented name type.";
+}
+
+expression_checker::info expression_checker::generate(
+    io::location location, const ast::literal_integer& i) {
+  if (i.value > std::numeric_limits<std::int32_t>::max()) {
+    io::fatal_message{module.name(), location, io::message::error}
+        << "integer literal exceeds the maximum allowed value for int32.";
+  }
+  const auto& result = add({location, std::int32_t(i.value)});
+  return {.category = info::rvalue, .result = &result};
+}
+
+expression_checker::info expression_checker::generate(
+    io::location location, const ast::literal_string&) {
+  io::fatal_message{module.name(), location, io::message::error}
+      << "string literals are unimplemented.";
+}
+
+expression_checker::info expression_checker::generate(
+    io::location location, const ast::literal_aggregate& a,
+    const semantics::ir::array_type& array) {
+  const auto& mem = alloc({location, array});
+  generate_into(mem.first, location, a, array);
+  return {.category = info::rvalue, .result = &mem};
+}
+
+expression_checker::info expression_checker::generate(
+    io::location location, const ast::literal_aggregate& a) {
+  const auto type = module.check_type(a.type);
+  if (const auto* array = type.get<semantics::ir::array_type>()) {
+    return generate(location, a, *array);
+  }
+  io::fatal_message{module.name(), location, io::message::error}
+      << "unimplemented aggregate literal type.";
+}
+
+expression_checker::info expression_checker::generate(
+    const ast::expression& e) {
+  return e.visit([&](const auto& x) { return generate(e.location(), x); });
+}
+
+void expression_checker::generate_into(semantics::ir::local address,
+                                       io::location location,
+                                       const ast::literal_aggregate& a,
+                                       const semantics::ir::array_type& array) {
+  // Array literals can either have delimited indices, e.g.
+  // `[256]bool{[42] = true}`, or they can have ordered bare values, e.g.
+  // `[3]int32{1, 2, 3}`. These two options cannot be mixed, and only the
+  // former option can specify a partial set of indices.
+  bool has_index = false;
+  bool has_bare = false;
+  for (const auto& argument : a.arguments) {
+    if (auto* e = std::get_if<ast::expression>(&argument)) {
+      if (has_index) {
+        io::fatal_message{module.name(), e->location(), io::message::error}
+            << "cannot mix bare expressions and indexed expressions in an "
+               "array literal.";
+      }
+      has_bare = true;
+      continue;
+    }
+    if (auto* f = std::get_if<ast::literal_aggregate::field_assignment>(
+            &argument)) {
+      io::fatal_message{module.name(), f->value.location(),
+                        io::message::error}
+          << "cannot have field assignments in an array literal.";
+    }
+    if (auto* i = std::get_if<ast::literal_aggregate::index_assignment>(
+            &argument)) {
+      if (has_bare) {
+        io::fatal_message{module.name(), i->value.location(),
+                          io::message::error}
+            << "cannot mix bare expressions and indexed expressions in an "
+               "array literal.";
+      }
+      has_index = true;
+    }
+  }
+  assert(!has_index || !has_bare);
+  if (has_bare) {
+    if (a.arguments.size() != array.size) {
+      io::message{module.name(), location, io::message::error}
+          << "unindexed array literals must initialize every value.";
+      io::fatal_message{module.name(), a.type.location(), io::message::note}
+          << "expected " << array.size << " initializers but got "
+          << a.arguments.size() << ".";
+    }
+    for (std::int32_t i = 0, n = a.arguments.size(); i < n; i++) {
+      const auto& argument = std::get<ast::expression>(a.arguments[i]);
+      const auto& index = add({argument.location(), i});
+      const auto& target = add(
+          array.element,
+          {argument.location(), semantics::ir::index{address, index.first}});
+      generate_into(target.first, argument);
+    }
+  }
+  io::fatal_message{module.name(), a.type.location(), io::message::error}
+      << "unimplemented array literal type.";
+}
+
+void expression_checker::generate_into(semantics::ir::local address,
+                                       const ast::expression& e) {
+  return e.visit(
+      [&](const auto& x) { generate_into(address, e.location(), x); });
 }
 
 export void check(const char* filename) {
